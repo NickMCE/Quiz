b2c049b33300f2acc45f360d201646520740867d
NIKHIL <138965707+NickMCE@users.noreply.github.com>
2024-07-15 13:50:10 +0530

    Quiz

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,94 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the capital of Haryana?",
+        options: ["Yamunanagar", "Panipat", "Gurgaon", "Chandigarh"],
+        answer: "Chandigarh",
+    },
+    {
+        id: 2,
+        question: "What is the capital of Punjab?",
+        options: ["Patiala", "Ludhiana", "Amritsar", "Chandigarh"],
+        answer: "Chandigarh",
+    },
+    {
+        id: 3,
+        question: "What is the capital of India?",
+        options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+        answer: "Delhi"
+    },
+    {
+        id: 4,
+        question: "What is the capital of Uttarakhand?",
+        options: ["Roorkee", "Haridwar", "Dehradun", "Nainital"],
+        answer: "Dehradun"
+    },
+    {
+        id: 5,
+        question: "What is the capital of Uttar Pradesh?",
+        options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+        answer: "Lucknow"
+    },
+    {
+        id: 6,
+        question: "What is the capital of Maharashtra?",
+        options: ["Pune", "Nagpur", "Mumbai", "Nashik"],
+        answer: "Mumbai"
+    },
+    {
+        id: 7,
+        question: "What is the capital of Karnataka?",
+        options: ["Mysore", "Mangalore", "Bangalore", "Hubli"],
+        answer: "Bangalore"
+    },
+    {
+        id: 8,
+        question: "What is the capital of Tamil Nadu?",
+        options: ["Coimbatore", "Madurai", "Chennai", "Salem"],
+        answer: "Chennai"
+    },
+    {
+        id: 9,
+        question: "What is the capital of West Bengal?",
+        options: ["Howrah", "Durgapur", "Asansol", "Kolkata"],
+        answer: "Kolkata"
+    },
+    {
+        id: 10,
+        question: "What is the capital of Gujarat?",
+        options: ["Vadodara", "Ahmedabad", "Surat", "Gandhinagar"],
+        answer: "Gandhinagar"
+    },
+    {
+        id: 11,
+        question: "What is the capital of Rajasthan?",
+        options: ["Jodhpur", "Udaipur", "Jaipur", "Ajmer"],
+        answer: "Jaipur"
+    },
+    {
+        id: 12,
+        question: "What is the capital of Bihar?",
+        options: ["Gaya", "Patna", "Muzaffarpur", "Bhagalpur"],
+        answer: "Patna"
+    },
+    {
+        id: 13,
+        question: "What is the capital of Madhya Pradesh?",
+        options: ["Indore", "Bhopal", "Gwalior", "Jabalpur"],
+        answer: "Bhopal"
+    },
+    {
+        id: 14,
+        question: "What is the capital of Kerala?",
+        options: ["Kochi", "Kozhikode", "Thrissur", "Thiruvananthapuram"],
+        answer: "Thiruvananthapuram"
+    },
+    {
+        id: 15,
+        question: "What is the capital of Andhra Pradesh?",
+        options: ["Vijayawada", "Visakhapatnam", "Tirupati", "Amaravati"],
+        answer: "Amaravati"
+    }
+];
+
+export default qBank;
